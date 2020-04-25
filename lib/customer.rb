78e7dc7ac19901eb205fad260c209814212f1709
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-25 21:42:49 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,15 +1,18 @@
 class Customer
   attr_accessor :name, :age
 
-@@all = []
+  @@all = []
 
-def initialize(name, age)
-  @name = name
-  @age = age
-  @@all << self
-end
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
 
-def self.all
-  @@all
-end
+  def self.all
+    @@all
+  end
+  def new_meal(waiter, total, tip=0)
+      Meal.new(waiter, self, total, tip)
+    end
 end
